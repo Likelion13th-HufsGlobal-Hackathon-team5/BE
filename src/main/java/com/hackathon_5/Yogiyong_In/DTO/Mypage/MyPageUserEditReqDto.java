e2b3e71c66185eda297166af8ffa1a110a622864
sunmin416 <rhtjsals0106@gmail.com>
2025-08-21 21:54:40 +0900

    [FIX] 마이페이지 및 로그인 토큰

--- src/main/java/com/hackathon_5/Yogiyong_In/DTO/Mypage/MyPageUserEditReqDto.java
+++ src/main/java/com/hackathon_5/Yogiyong_In/DTO/Mypage/MyPageUserEditReqDto.java
@@ -1,10 +1,21 @@
 package com.hackathon_5.Yogiyong_In.DTO.Mypage;
 
-import com.hackathon_5.Yogiyong_In.domain.User;
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter @Setter @NoArgsConstructor
 public class MyPageUserEditReqDto {
+
+    @NotBlank(message = "이 필드는 필수입니다.")
     private String nickname;
-    private Integer birthYear;
+
+    @NotNull(message = "이 필드는 필수입니다.")
+    private Integer birthyear;
+
+    private String password;
+    private String passwordConfirm;
+    private String profileImageUrl;
 }
